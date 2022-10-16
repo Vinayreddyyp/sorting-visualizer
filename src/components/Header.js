8eea917f09def4e54d61ebe1d57ce1cedeb34f81
vinayreddyyp <ypvinayreddy@gmail.com>
2022-10-15 23:42:55 -0700

    add header to the app

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import { Button, Container, SortWrapper } from "./header.style";
+
+import Box from "@mui/material/Box";
+import React from "react";
+import Slider from "@mui/material/Slider";
+
+const Header = (props) => {
+	const { valuetext, value, generateBubbleSort,  generateSelectionSort } = props;
+	return (
+		<Container>
+			<Button>Generate New Array</Button>
+			<span style={{ color: "white" }}>
+				<p>Select Input Range</p>
+			</span>
+			<Box sx={{ width: 200 }}>
+				<Slider
+					aria-label="Temperature"
+					defaultValue={30}
+					getAriaValueText={valuetext}
+					step={10}
+					marks
+					min={10}
+					max={50}
+				/>
+			</Box>
+			<span style={{ color: "white" }}>{value}</span>
+			<SortWrapper>
+				<Button onClick={generateBubbleSort}>Bubble Sort</Button>
+				<Button onClick={generateSelectionSort}>Selection Sort</Button>
+				<Button>Quick Sort</Button>
+				<Button>Merge Sort</Button>
+			</SortWrapper>
+		</Container>
+	);
+};
+
+export default Header;
