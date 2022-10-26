61cb6225f00502d9eb0d9bbeed5bb0f98e62e0e1
vinayreddyyp <ypvinayreddy@gmail.com>
2022-10-25 18:06:51 -0700

    add Merge sort to the app

--- src/algorithms/MergeSort.js
+++ src/algorithms/MergeSort.js
@@ -6,11 +6,7 @@ const MergeSort = (props) => {
 	const { arr, isSort, setArr, setCurrentIdx, setNextIdx, setLastSortedIdx } =
 		props;
 
-	const [arrayCopy, setArrayCopy] = useState([]);
-	const [result, setResult] = useState([]);
-
-	const sorting = async (sortArr) => {
-		console.log("arr in the sort", sortArr);
+	const sorting = (sortArr) => {
 		const originalArray = [...sortArr];
 		if (sortArr.length <= 1) return sortArr;
 		let mid = Math.floor(sortArr.length / 2);
@@ -25,95 +21,65 @@ const MergeSort = (props) => {
 		sorting(arr);
 	}, []);
 
-	/* const merge = async (arr1, arr2, originalArray) => {
-		let results = [];
+	/* const setAnimationIdx = (i, j) => {
+		console.log("i,j", i, j);
+	};
 
-		let i = 0;
-
-		let j = 0;
-		console.log("j in the merge", i, j);
-
-		while (i < arr1.length && j < arr2.length) {
-			if (arr2[j] > arr1[i]) {
-				debugger;
-				results.push(arr1[i]);
-				let newLeftIndex = arr.indexOf(arr1[i]);
-				console.log("newLeftIdex", newLeftIndex, arr[newLeftIndex]);
-				let newRightIndex = arr.indexOf(arr[j]);
-				console.log("newRightIndex", newRightIndex, arr[newRightIndex]);
-				[originalArray[newRightIndex], originalArray[newLeftIndex]] = [
-					originalArray[newLeftIndex],
-					originalArray[newRightIndex],
-				];
-				setArr([...arr]);
-				setCurrentIdx(newLeftIndex);
-				setNextIdx(newRightIndex);
-				console.log("arr in the sorting", arr);
-				i++;
-			} else {
-				results.push(arr2[j]);
-
-				j++;
-				let newLeftIndex = arr.indexOf(arr1[i]);
-				console.log("newLeftIdex", newLeftIndex, arr[newLeftIndex]);
-				let newRightIndex = arr.indexOf(arr[j]);
-				console.log("newLeftIdex", newRightIndex, arr[newRightIndex]);
-
-				setArr([...arr, arr[newLeftIndex], arr[newRightIndex]]);
-				console.log("arr in the sorting", arr);
-			}
-		}
-		while (i < arr1.length) {
-			results.push(arr1[i]);
-
-			i++;
-		}
-		while (j < arr2.length) {
-			results.push(arr2[j]);
-
-			j++;
-		}
-		console.log("results", results);
-		setArr(originalArray);
-		results = results.concat(arr1.slice(i)).concat(arr2.slice(j));
-		console.log("results", results);
-		return results;
+	const setIdx = (i, j) => {
+		console.log("i,j", i, j);
+		debugger;
+		setI(i);
+		setJ(j);
 	}; */
 
-	const animations = async (arr) => {
-		const resultArr = arr;
-		for (let i = 0; i <= resultArr.length; i++) {}
+	const animations = async (ra) => {
+		const resultArr = ra;
 
-		setArr([...resultArr]);
+		for (let i = 0; i <= resultArr.length; i++) {
+			console.log("arr[i]====resultArr[i]", arr[i], resultArr[i]);
+			if (arr[i] === resultArr[i]) {
+				debugger;
+				const sortedIdx = arr.indexOf(resultArr[i]);
+				setLastSortedIdx(sortedIdx);
+			}
+			await sleep(200);
+		}
+		setCurrentIdx(null);
+		setNextIdx(null);
 	};
 
 	const merge = (leftArr, rightArr, originalArray) => {
+		console.log("arr in the merge", arr);
 		let resultArray = [],
 			leftIndex = 0,
 			rightIndex = 0;
 
 		while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
 			if (leftArr[leftIndex] < rightArr[rightIndex]) {
-				debugger;
-				let newLeftIndex = originalArray.indexOf(leftArr[leftIndex]);
-				let newRightIndex = originalArray.indexOf(rightArr[rightIndex]);
-				[originalArray[newLeftIndex], originalArray[newRightIndex]] = [
-					originalArray[newRightIndex],
-					originalArray[newLeftIndex],
-				];
-				console.log(
-					"[originalArray[newLeftIndex], originalArray[newRightIndex]]",
-					[originalArray[newLeftIndex], originalArray[newRightIndex]]
-				);
+				let arrIdxL = arr.indexOf(leftArr[leftIndex]);
+				let arrIdxR = arr.indexOf(rightArr[rightIndex]);
+				console.log("arrIDx in the merge", arrIdxL, arrIdxR);
+				/* setCurrentIdx(arrIdxL);
+				setNextIdx(arrIdxR); */
+				setTimeout(() => {
+					setCurrentIdx(arrIdxL);
+					setNextIdx(arrIdxR);
+				}, 300);
+
 				resultArray.push(leftArr[leftIndex]);
 				leftIndex++;
+
+				//setAnimationIdx(newLeftIndex, newRightIndex);
 			} else {
-				let newLeftIndex = originalArray.indexOf(leftArr[leftIndex]);
-				let newRightIndex = originalArray.indexOf(rightArr[rightIndex]);
-				[originalArray[newRightIndex], originalArray[newLeftIndex]] = [
-					originalArray[newLeftIndex],
-					originalArray[newRightIndex],
-				];
+				let arrIdxL = arr.indexOf(leftArr[leftIndex]);
+				let arrIdxR = arr.indexOf(rightArr[rightIndex]);
+				console.log("arrIDx in the merge", arrIdxL, arrIdxR);
+
+				setTimeout(() => {
+					setCurrentIdx(arrIdxL);
+					setNextIdx(arrIdxR);
+				}, 300);
+
 				resultArray.push(rightArr[rightIndex]);
 				rightIndex++;
 			}
@@ -122,9 +88,10 @@ const MergeSort = (props) => {
 		resultArray = resultArray
 			.concat(leftArr.slice(leftIndex))
 			.concat(rightArr.slice(rightIndex));
-		console.log("resultArray", resultArray);
-
 		animations(resultArray);
+
+		setArr([...resultArray]);
+
 		return resultArray;
 	};
 };
